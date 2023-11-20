e2cd137a92658e44a146f17de6c218bb7b2fc423
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2023-11-20 11:14:07 +0530

    project setup

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,24 @@
+import { Box, Button, Grid, Typography } from "@mui/material";
+
+const Header = () => {
+  return (
+    <Box p={10} bgcolor="secondary.main" color="white">
+      <Grid container>
+        <Grid item xs={12}>
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Typography variant="h4">Open Job Listing</Typography>
+            <Button variant="contained" disableElevation>
+              Post a Job
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Header;
