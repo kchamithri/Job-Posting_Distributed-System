c3d7be1914782bc5f1323c5ec79f7af6cc81ea89
Thushan Dodanwela <dodanwelathushan@gmail.com>
2023-11-23 05:11:25 +0530

    part 03 - Thushan

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { ThemeProvider } from "@mui/material";
+import { Grid, ThemeProvider } from "@mui/material";
 import theme from "./theme/theme";
 import Header from "./Components/Header";
+import SearchBar from "./Components/SearchBar";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <Grid container justify="center">
+        <Grid item xs={10}>
+          <SearchBar />
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
 }
---
+++ src/Components/SearchBar/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Box, Button, Select, MenuItem, makeStyles } from "@material-ui/core";
+
+const useStyle = makeStyles({
+  wrapper: {
+    backgroundColor: "#fff",
+    display: "flex",
+    boxShadow: "0px 1px 5px rgba(0,0,0,0.1)",
+    borderRadius: "5px",
+    "& > *": {
+      flex: 1,
+      height: "45px",
+      margin: "8px",
+    },
+  },
+});
+
+export default (props) => {
+  const classes = useStyle();
+  return (
+    <Box p={2} mt={-5} mb={2} className={classes.wrapper}>
+      <Select disableUnderline variant="filled" defaultValue="FullTime">
+        <MenuItem value="FullTime">Full time</MenuItem>
+        <MenuItem value="PartTime">Part time</MenuItem>
+        <MenuItem value="Contract">Contract</MenuItem>
+      </Select>
+
+      <Select disableUnderline variant="filled" defaultValue="Remote">
+        <MenuItem value="Remote">Remote</MenuItem>
+        <MenuItem value="In-Office">In-Office</MenuItem>
+      </Select>
+
+      <Button variant="contained" color="primary" disableElevation>
+        Search
+      </Button>
+    </Box>
+  );
+};
