6c4a3312af1e3528b43fb9b7932593c650d6fc99
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2024-01-04 00:02:35 +0530

    part 7

--- src/Components/Job/NewJobModel.jsx
+++ src/Components/Job/NewJobModel.jsx
@@ -2,18 +2,58 @@ import React from "react";
 import {
   Box,
   Dialog,
+  DialogActions,
   DialogContent,
   DialogTitle,
   FilledInput,
   Grid,
   MenuItem,
   Select,
+  Typography,
+  makeStyles,
+  Button,
+  IconButton,
 } from "@material-ui/core";
+import CloseIcon from "@mui/icons-material/Close";
+
+const useStyle = makeStyles((theme) => ({
+  skillChip: {
+    margin: theme.spacing(0.5),
+    padding: theme.spacing(0.75),
+    fontSize: "14.5px",
+    borderRadius: "5px",
+    fontWeight: 600,
+    border: `1px solid ${theme.palette.secondary.main}`,
+    cursor: "pointer",
+
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.main,
+      color: "#ffffff",
+    },
+  },
+}));
 
 export default (props) => {
+  const classes = useStyle();
+  const skills = [
+    "Javascript",
+    "React js",
+    "Node js",
+    "Vue js",
+    "Firebase",
+    "MongoDB",
+    "SQL",
+  ];
   return (
     <Dialog open={true} fullWidth>
-      <DialogTitle>Post Job</DialogTitle>
+      <DialogTitle>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          Post Job
+          <IconButton>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+      </DialogTitle>
       <DialogContent>
         <Grid container spacing={2}>
           <Grid item xs={6}>
@@ -69,7 +109,33 @@ export default (props) => {
             />
           </Grid>
         </Grid>
+        <Box mt={2}>
+          <Typography>Skills</Typography>
+          <Box display="flex">
+            {skills.map((skill) => {
+              return (
+                <Box className={classes.skillChip} key={skill}>
+                  {skill}
+                </Box>
+              );
+            })}
+          </Box>
+        </Box>
       </DialogContent>
+      <DialogActions>
+        <Box
+          color="red"
+          width="100%"
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography variant="caption">*Required Fields</Typography>
+          <Button variant="contained" disableElevation color="primary">
+            Post Job
+          </Button>
+        </Box>
+      </DialogActions>
     </Dialog>
   );
 };
