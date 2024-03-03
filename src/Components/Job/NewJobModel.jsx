ce7dbae477240fc0a748862d35e11b69180f783b
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2024-03-03 15:12:42 +0530

    updated

--- src/Components/Job/NewJobModel.jsx
+++ src/Components/Job/NewJobModel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useMemo } from "react";
+import React, { useState, useRef, useMemo } from "react";
 import {
   Box,
   Dialog,
@@ -16,9 +16,8 @@ import {
 } from "@material-ui/core";
 import CloseIcon from "@mui/icons-material/Close";
 import { CircularProgress } from "@mui/material";
-import { useCallback } from "react";
 
-const useStyle = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
   skillChip: {
     margin: theme.spacing(0.5),
     padding: theme.spacing(0.75),
@@ -42,7 +41,6 @@ const useStyle = makeStyles((theme) => ({
 const NewJobModel = (props) => {
   const [loading, setLoading] = useState(false);
   const dateRef = useRef("");
-  // const [newJobModal, setNewJobModal] = useState(false);
   const initState = useMemo(
     () => ({
       companyName: "",
@@ -58,11 +56,27 @@ const NewJobModel = (props) => {
     []
   );
 
-  useEffect(() => {
-    dateRef.current = new Date().toISOString();
-  }, []);
-
   const [jobDetails, setJobDetails] = useState(initState);
+  const classes = useStyles();
+  const skills = [
+    "Javascript",
+    "React js",
+    "Node js",
+    "Vue js",
+    "Firebase",
+    "MongoDB",
+    "SQL",
+  ];
+
+  const BASE_URL =
+    "https://job-posting-app-front-door-hyaufmbbe7hug4dk.z02.azurefd.net/api";
+  const JOB_API_URL = BASE_URL + "/add-job";
+
+  const closeModalCallback = () => {
+    setJobDetails(initState);
+    setLoading(false);
+    props.closeModal();
+  };
 
   const handleChange = (e) => {
     e.persist();
@@ -73,193 +87,52 @@ const NewJobModel = (props) => {
   };
 
   const addRemoveSkill = (skill) =>
-    jobDetails.skills.includes(skill)
-      ? setJobDetails((oldState) => ({
-          ...oldState,
-          skills: oldState.skills.filter((s) => s !== skill),
-        }))
-      : setJobDetails((oldState) => ({
-          ...oldState,
-          skills: oldState.skills.concat(skill),
-        }));
-  const classes = useStyle();
-  const skills = [
-    "Javascript",
-    "React js",
-    "Node js",
-    "Vue js",
-    "Firebase",
-    "MongoDB",
-    "SQL",
-  ];
-  console.log(jobDetails);
+    setJobDetails((oldState) => ({
+      ...oldState,
+      skills: oldState.skills.includes(skill)
+        ? oldState.skills.filter((s) => s !== skill)
+        : oldState.skills.concat(skill),
+    }));
 
-  const BASE_URL =
-    "https://job-posting-app-front-door-hyaufmbbe7hug4dk.z02.azurefd.net/api";
-  const JOB_API_URL = BASE_URL + "/add-job";
+  const postJob = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(JOB_API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jobDetails),
+      });
 
-  const [isPostingJob, setIsPostingJob] = useState(false);
-
-  // Destructure the props object outside useCallback
-  const { closeModal } = props;
-
-  // Wrap closeModal in useCallback and refer to the specific prop inside
-  const closeModalCallback = useCallback(() => {
-    setJobDetails(initState);
-    setLoading(false);
-    closeModal();
-  }, [closeModal, initState]);
-
-  useEffect(() => {
-    const postJob = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(JOB_API_URL, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(jobDetails),
-        });
-
-        if (response.ok) {
-          console.log("Job posted successfully");
-        } else {
-          const errorMessage = await response.text();
-          console.error("Failed to post job:", errorMessage);
-        }
-      } catch (error) {
-        console.error("Error posting job:", error);
-      } finally {
-        // closeModal();
-        closeModalCallback();
-        // Reset the state variable to indicate that the job posting is complete
-        setIsPostingJob(false);
-        // props.onNewJobPosted();
+      if (response.ok) {
+        console.log("Job posted successfully");
+      } else {
+        const errorMessage = await response.text();
+        console.error("Failed to post job:", errorMessage);
       }
-    };
-
-    if (isPostingJob) {
-      // Trigger the job posting when isPostingJob is true
-      postJob();
+    } catch (error) {
+      console.error("Error posting job:", error);
+    } finally {
+      closeModalCallback();
+      setLoading(false);
     }
-  }, [isPostingJob, JOB_API_URL, jobDetails, closeModalCallback]); // Dependency array: watch for changes in isPostingJob or jobDetails
+  };
 
   return (
     <Dialog open={props.newJobModal} fullWidth>
       <DialogTitle>
         <Box display="flex" justifyContent="space-between" alignItems="center">
           Post Job
-          <IconButton onClick={closeModal}>
+          <IconButton onClick={props.closeModal}>
             <CloseIcon />
           </IconButton>
         </Box>
       </DialogTitle>
       <DialogContent>
         <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <FilledInput
-              onChange={handleChange}
-              name="title"
-              value={jobDetails.title}
-              autoComplete="off"
-              placeholder="Job Title*"
-              disableUnderline
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <Select
-              onChange={handleChange}
-              name="type"
-              value={jobDetails.type}
-              fullWidth
-              disableUnderline
-              variant="filled"
-            >
-              <MenuItem value="FullTime">Full time</MenuItem>
-              <MenuItem value="PartTime">Part time</MenuItem>
-              <MenuItem value="Contract">Contract</MenuItem>
-            </Select>
-          </Grid>
-          <Grid item xs={6}>
-            <FilledInput
-              onChange={handleChange}
-              name="companyName"
-              value={jobDetails.companyName}
-              autoComplete="off"
-              placeholder="Company Name*"
-              disableUnderline
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <FilledInput
-              onChange={handleChange}
-              name="companyUrl"
-              value={jobDetails.companyUrl}
-              autoComplete="off"
-              placeholder="Company URL*"
-              disableUnderline
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <Select
-              onChange={handleChange}
-              name="location"
-              value={jobDetails.location}
-              fullWidth
-              disableUnderline
-              variant="filled"
-              // defaultValue="FullTime"
-            >
-              <MenuItem value="Remote">Remote</MenuItem>
-              <MenuItem value="On-Site">On-Site</MenuItem>
-            </Select>
-          </Grid>
-          <Grid item xs={6}>
-            <FilledInput
-              onChange={handleChange}
-              name="link"
-              value={jobDetails.link}
-              placeholder="Job Link*"
-              disableUnderline
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FilledInput
-              onChange={handleChange}
-              name="description"
-              value={jobDetails.description}
-              autoComplete="off"
-              placeholder="Job Description*"
-              disableUnderline
-              fullWidth
-              multiline
-              rows={3}
-            />
-          </Grid>
+          {/* Job details inputs */}
         </Grid>
-        <Box mt={2}>
-          <Typography>Skills</Typography>
-          <Box display="flex">
-            {skills.map((skill) => {
-              return (
-                <Box
-                  onClick={() => addRemoveSkill(skill)}
-                  className={`${classes.skillChip} ${
-                    jobDetails.skills.includes(skill) && classes.included
-                  }`}
-                  key={skill}
-                >
-                  {skill}
-                </Box>
-              );
-            })}
-          </Box>
-        </Box>
       </DialogContent>
       <DialogActions>
         <Box
@@ -274,8 +147,7 @@ const NewJobModel = (props) => {
             variant="contained"
             disableElevation
             color="primary"
-            // onClick={handlePostJob}
-            onClick={() => setIsPostingJob(true)}
+            onClick={postJob} // Call postJob directly on button click
             disabled={loading}
           >
             {loading ? (
