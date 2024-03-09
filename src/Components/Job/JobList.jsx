4497f040d3d872d6a48e128bb83eec6cf719cc50
LakshanRashminda <106988246+LakshanRashminda@users.noreply.github.com>
2024-03-09 20:57:43 +0530

    Status

--- src/Components/Job/JobList.jsx
+++ src/Components/Job/JobList.jsx
@@ -1,37 +1,63 @@
-import React, { useEffect, useState } from "react";
-import { Grid, ThemeProvider } from "@mui/material";
-import theme from "../../theme/theme";
-import Header from "../Header";
-import SearchBar from "../SearchBar";
-import JobCard from "../Job/JobCard";
-import { Box, CircularProgress } from "@material-ui/core";
-import ViewJobModal from "./VIewJobModal";
+import React, { useEffect, useState } from 'react';
+import { Grid, ThemeProvider } from '@mui/material';
+import theme from '../../theme/theme';
+import Header from '../Header';
+import SearchBar from '../SearchBar';
+import JobCard from '../Job/JobCard';
+import { Box, CircularProgress } from '@material-ui/core';
+import ViewJobModal from './VIewJobModal';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function JobList({ jobs, setJobs, loading, setLoading }) {
   const BASE_URL =
-    "https://job-posting-app-front-door-hyaufmbbe7hug4dk.z02.azurefd.net/api";
+    'https://job-posting-app-front-door-hyaufmbbe7hug4dk.z02.azurefd.net/api';
 
-  const JOBS_API_URL = BASE_URL + "/get-jobs";
+  const JOBS_API_URL = BASE_URL + '/get-jobs';
 
   const [viewJob, setViewJob] = useState({});
 
-  const [jobSearch, setJobSearch]= useState({
-    type: "Full time",
-    location: "Remote",
-  })
+  const [jobSearch, setJobSearch] = useState({
+    type: 'Full time',
+    location: 'Remote',
+  });
 
+  // useEffect(() => {
+  //   fetch(JOBS_API_URL)
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       setJobs(data);
+  //       console.log(data);
+  //       setLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching jobs:", error);
+  //       setLoading(false); // Set loading to false even in case of error
+  //     });
+  // }, [JOBS_API_URL, setJobs, setLoading]);
 
   useEffect(() => {
     fetch(JOBS_API_URL)
       .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
+        if (response.status === 200) {
+          toast.success("Success");
+          return response.json();
+        } else if (response.status === 403) {
+          toast.error("Server down. Please try again later.");
+          throw new Error("Server down");
+        } else {
+          toast.error("Network error. Please try again.");
+          throw new Error("Network error");
         }
-        return response.json();
       })
       .then((data) => {
         setJobs(data);
-        console.log(data);
         setLoading(false);
       })
       .catch((error) => {
@@ -46,40 +72,52 @@ function JobList({ jobs, setJobs, loading, setLoading }) {
       ...oldState,
       [e.target.name]: e.target.value,
     }));
-  }; 
+  };
 
   const jobSearchhandler = (type, location) => {
-    console.log("searching")
+    console.log('searching');
     //filter jobs based on type and location from  fetch jobs_api_url
-    console.log("searching for jobs");
+    console.log('searching for jobs');
     fetch(JOBS_API_URL)
       .then((response) => response.json())
       .then((data) => {
-        const filteredJobs = data.filter((job) =>
-          job.type.toLowerCase().includes(type.toLowerCase()) 
-          && job.location.toLowerCase().includes(location.toLowerCase())
+        const filteredJobs = data.filter(
+          (job) =>
+            job.type.toLowerCase().includes(type.toLowerCase()) &&
+            job.location.toLowerCase().includes(location.toLowerCase())
         );
         setJobs(filteredJobs);
       })
       .catch((error) => {
-        console.log("error fetching:", error);
+        console.log('error fetching:', error);
       });
-  }
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      <ViewJobModal job={viewJob} closeModal={()=>setViewJob({})}/>
+      <ViewJobModal job={viewJob} closeModal={() => setViewJob({})} />
+      <ToastContainer />
       <Grid container justifyContent="center">
         <Grid item xs={10}>
-        <SearchBar onclick={()=>jobSearchhandler(jobSearch.type, jobSearch.location)} handleChange={handleChange} jobSearch={jobSearch}/>
+          <SearchBar
+            onclick={() => jobSearchhandler(jobSearch.type, jobSearch.location)}
+            handleChange={handleChange}
+            jobSearch={jobSearch}
+          />
           {loading ? (
             <Box display="flex" justifyContent="center">
               <CircularProgress />
             </Box>
           ) : (
             jobs.map((data) => {
-              return <JobCard open={()=>setViewJob(data)} key={data.id} {...data} />;
+              return (
+                <JobCard
+                  open={() => setViewJob(data)}
+                  key={data.id}
+                  {...data}
+                />
+              );
             })
           )}
         </Grid>
--- src/Components/Job/NewJobModel.jsx
+++ src/Components/Job/NewJobModel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Box,
   Dialog,
@@ -13,31 +13,33 @@ import {
   makeStyles,
   Button,
   IconButton,
-} from "@material-ui/core";
-import CloseIcon from "@mui/icons-material/Close";
-import { CircularProgress } from "@mui/material";
-import { useMsal } from "@azure/msal-react";
-import { loginRequest } from "../../authConfig";
-import { useNavigate } from "react-router";
+} from '@material-ui/core';
+import CloseIcon from '@mui/icons-material/Close';
+import { CircularProgress } from '@mui/material';
+import { useMsal } from '@azure/msal-react';
+import { loginRequest } from '../../authConfig';
+import { useNavigate } from 'react-router';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const useStyles = makeStyles((theme) => ({
   skillChip: {
     margin: theme.spacing(0.5),
     padding: theme.spacing(0.75),
-    fontSize: "14.5px",
-    borderRadius: "5px",
+    fontSize: '14.5px',
+    borderRadius: '5px',
     fontWeight: 600,
     border: `1px solid ${theme.palette.secondary.main}`,
-    cursor: "pointer",
+    cursor: 'pointer',
 
-    "&:hover": {
+    '&:hover': {
       backgroundColor: theme.palette.secondary.main,
-      color: "#ffffff",
+      color: '#ffffff',
     },
   },
   included: {
     backgroundColor: theme.palette.secondary.main,
-    color: "#ffffff",
+    color: '#ffffff',
   },
 }));
 
@@ -47,32 +49,32 @@ const NewJobModel = (props) => {
   const navigateTo = useNavigate();
 
   const initState = {
-    companyName: "",
-    companyUrl: "",
-    link: "",
-    location: "",
+    companyName: '',
+    companyUrl: '',
+    link: '',
+    location: '',
     postedOn: new Date().toISOString(),
     skills: [],
-    title: "",
-    type: "",
-    description: "",
+    title: '',
+    type: '',
+    description: '',
   };
 
   const [jobDetails, setJobDetails] = useState(initState);
   const classes = useStyles();
   const skills = [
-    "Javascript",
-    "React js",
-    "Node js",
-    ".NET",
-    "Firebase",
-    "MongoDB",
-    "SQL",
+    'Javascript',
+    'React js',
+    'Node js',
+    '.NET',
+    'Firebase',
+    'MongoDB',
+    'SQL',
   ];
 
   const BASE_URL =
-    "https://job-posting-app-front-door-hyaufmbbe7hug4dk.z02.azurefd.net/api";
-  const JOB_API_URL = BASE_URL + "/add-job";
+    'https://job-posting-app-front-door-hyaufmbbe7hug4dk.z02.azurefd.net/api';
+  const JOB_API_URL = BASE_URL + '/add-job';
 
   const closeModalCallback = () => {
     setJobDetails(initState);
@@ -107,171 +109,179 @@ const NewJobModel = (props) => {
       });
 
       const response = await fetch(JOB_API_URL, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${tokenResponse.accessToken}`,
         },
         body: JSON.stringify(jobDetails),
       });
 
       if (response.ok) {
-        console.log("Job posted successfully");
+        toast.success('Job posted successfully');
+        console.log('Job posted successfully');
       } else {
         const errorMessage = await response.text();
-        console.error("Failed to post job:", errorMessage);
+        console.error('Failed to post job:', errorMessage);
       }
     } catch (error) {
-      console.error("Error posting job:", error);
+      console.error('Error posting job:', error);
     } finally {
       closeModalCallback();
       setLoading(false);
-      navigateTo("/");
+      navigateTo('/');
     }
   };
 
   return (
-    <Dialog open={props.newJobModal} fullWidth>
-      <DialogTitle>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          Post Job
-          <IconButton onClick={props.closeModal}>
-            <CloseIcon />
-          </IconButton>
-        </Box>
-      </DialogTitle>
-      <DialogContent>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <FilledInput
-              onChange={handleChange}
-              name="title"
-              value={jobDetails.title}
-              autoComplete="off"
-              placeholder="Job Title*"
-              disableUnderline
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <Select
-              onChange={handleChange}
-              name="type"
-              value={jobDetails.type}
-              label="Type"
-              fullWidth
-              disableUnderline
-              variant="filled"
-            >
-              <MenuItem value="FullTime">Full time</MenuItem>
-              <MenuItem value="PartTime">Part time</MenuItem>
-              <MenuItem value="Contract">Contract</MenuItem>
-            </Select>
-          </Grid>
-          <Grid item xs={6}>
-            <FilledInput
-              onChange={handleChange}
-              name="companyName"
-              value={jobDetails.companyName}
-              autoComplete="off"
-              placeholder="Company Name*"
-              disableUnderline
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <FilledInput
-              onChange={handleChange}
-              name="companyUrl"
-              value={jobDetails.companyUrl}
-              autoComplete="off"
-              placeholder="Company URL*"
-              disableUnderline
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <Select
-              onChange={handleChange}
-              name="location"
-              value={jobDetails.location}
-              fullWidth
-              disableUnderline
-              variant="filled"
-              // defaultValue="FullTime"
-            >
-              <MenuItem value="Remote">Remote</MenuItem>
-              <MenuItem value="On-Site">On-Site</MenuItem>
-            </Select>
-          </Grid>
-          <Grid item xs={6}>
-            <FilledInput
-              onChange={handleChange}
-              name="link"
-              value={jobDetails.link}
-              placeholder="Job Link*"
-              disableUnderline
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FilledInput
-              onChange={handleChange}
-              name="description"
-              value={jobDetails.description}
-              autoComplete="off"
-              placeholder="Job Description*"
-              disableUnderline
-              fullWidth
-              multiline
-              rows={3}
-            />
-          </Grid>
-        </Grid>
-        <Box mt={2}>
-          <Typography>Skills</Typography>
-          <Box display="flex">
-            {skills.map((skill) => {
-              return (
-                <Box
-                  onClick={() => addRemoveSkill(skill)}
-                  className={`${classes.skillChip} ${
-                    jobDetails.skills.includes(skill) && classes.included
-                  }`}
-                  key={skill}
-                >
-                  {skill}
-                </Box>
-              );
-            })}
-          </Box>
-        </Box>
-      </DialogContent>
-      <DialogActions>
-        <Box
-          color="red"
-          width="100%"
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Typography variant="caption">*Required Fields</Typography>
-          <Button
-            variant="contained"
-            disableElevation
-            color="primary"
-            onClick={postJob} // Call postJob directly on button click
-            disabled={loading}
+    <>
+      <ToastContainer />
+      <Dialog open={props.newJobModal} fullWidth>
+        <DialogTitle>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
           >
-            {loading ? (
-              <CircularProgress color="secondary" size={22} />
-            ) : (
-              "Post Job"
-            )}
-          </Button>
-        </Box>
-      </DialogActions>
-    </Dialog>
+            Post Job
+            <IconButton onClick={props.closeModal}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+        </DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <FilledInput
+                onChange={handleChange}
+                name="title"
+                value={jobDetails.title}
+                autoComplete="off"
+                placeholder="Job Title*"
+                disableUnderline
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Select
+                onChange={handleChange}
+                name="type"
+                value={jobDetails.type}
+                label="Type"
+                fullWidth
+                disableUnderline
+                variant="filled"
+              >
+                <MenuItem value="FullTime">Full time</MenuItem>
+                <MenuItem value="PartTime">Part time</MenuItem>
+                <MenuItem value="Contract">Contract</MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={6}>
+              <FilledInput
+                onChange={handleChange}
+                name="companyName"
+                value={jobDetails.companyName}
+                autoComplete="off"
+                placeholder="Company Name*"
+                disableUnderline
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <FilledInput
+                onChange={handleChange}
+                name="companyUrl"
+                value={jobDetails.companyUrl}
+                autoComplete="off"
+                placeholder="Company URL*"
+                disableUnderline
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Select
+                onChange={handleChange}
+                name="location"
+                value={jobDetails.location}
+                fullWidth
+                disableUnderline
+                variant="filled"
+                // defaultValue="FullTime"
+              >
+                <MenuItem value="Remote">Remote</MenuItem>
+                <MenuItem value="On-Site">On-Site</MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={6}>
+              <FilledInput
+                onChange={handleChange}
+                name="link"
+                value={jobDetails.link}
+                placeholder="Job Link*"
+                disableUnderline
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FilledInput
+                onChange={handleChange}
+                name="description"
+                value={jobDetails.description}
+                autoComplete="off"
+                placeholder="Job Description*"
+                disableUnderline
+                fullWidth
+                multiline
+                rows={3}
+              />
+            </Grid>
+          </Grid>
+          <Box mt={2}>
+            <Typography>Skills</Typography>
+            <Box display="flex">
+              {skills.map((skill) => {
+                return (
+                  <Box
+                    onClick={() => addRemoveSkill(skill)}
+                    className={`${classes.skillChip} ${
+                      jobDetails.skills.includes(skill) && classes.included
+                    }`}
+                    key={skill}
+                  >
+                    {skill}
+                  </Box>
+                );
+              })}
+            </Box>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Box
+            color="red"
+            width="100%"
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Typography variant="caption">*Required Fields</Typography>
+            <Button
+              variant="contained"
+              disableElevation
+              color="primary"
+              onClick={postJob} // Call postJob directly on button click
+              disabled={loading}
+            >
+              {loading ? (
+                <CircularProgress color="secondary" size={22} />
+              ) : (
+                'Post Job'
+              )}
+            </Button>
+          </Box>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
